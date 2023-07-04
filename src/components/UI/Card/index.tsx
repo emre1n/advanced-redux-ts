d09cfe2e2f2d826a89f7ebf0aed7304bb5e042ee
emre1n <emre1n@protonmail.com>
2023-07-04 13:17:00 +0300

    fix Card component className prop

--- src/components/UI/Card/index.tsx
+++ src/components/UI/Card/index.tsx
@@ -1,11 +1,16 @@
 import styles from './styles.module.css';
 
 interface CardProps {
+  className?: string;
   children: JSX.Element;
 }
 
-const Card = ({ children }: CardProps) => {
-  return <section className={`${styles.card}`}>{children}</section>;
+const Card = ({ children, className }: CardProps) => {
+  return (
+    <section className={`${styles.card} ${className ? className : ''}`}>
+      {children}
+    </section>
+  );
 };
 
 export default Card;
